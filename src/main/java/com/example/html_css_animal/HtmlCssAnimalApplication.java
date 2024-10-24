cba361588ee8b0711e810346c253b63c6fb56119
Lina <lial0001.stud.kea.dk>
2024-10-24 19:11:12 +0200

    Initial commit

---
+++ src/main/java/com/example/html_css_animal/HtmlCssAnimalApplication.java
@@ -0,0 +1,13 @@
+package com.example.html_css_animal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlCssAnimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlCssAnimalApplication.class, args);
+	}
+
+}
