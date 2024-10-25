a7164037b251d211e6c9d4d8ade486becc814f52
Lina <lial0001.stud.kea.dk>
2024-10-25 12:29:18 +0200

    sidste commit

--- src/main/java/com/example/html_css_animal/HtmlCssAnimalApplication.java
+++ src/main/java/com/example/html_css_animal/HtmlCssAnimalApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HtmlCssAnimalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HtmlCssAnimalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HtmlCssAnimalApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/html_css_animal/HtmlCssAnimalApplicationTests.java
+++ src/test/java/com/example/html_css_animal/HtmlCssAnimalApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HtmlCssAnimalApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
